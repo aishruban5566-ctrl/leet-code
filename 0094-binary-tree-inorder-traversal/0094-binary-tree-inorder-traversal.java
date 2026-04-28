7a5155421f547facc56787895428e885d1975234
aishruban5566-ctrl <aishruban5566@gmail.com>
2026-04-28 10:25:20 +0530

    Time: 0 ms (100%), Space: 43.1 MB (69.75%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    void inorder(TreeNode root, List<Integer> result) {
+        if (root == null) return;
+
+        inorder(root.left, result);
+        result.add(root.val);
+        inorder(root.right, result);
+    }
+}
